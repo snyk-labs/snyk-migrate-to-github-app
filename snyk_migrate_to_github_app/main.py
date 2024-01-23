a61b51ffc9b5877027d7b6c25759a739e3c0bbcf
Troy Havelock <troy.havelock@snyk.io>
2024-01-23 15:25:12 -0500

    Adding support for eu and au tenants

--- snyk_migrate_to_github_app/main.py
+++ snyk_migrate_to_github_app/main.py
@@ -13,9 +13,15 @@ from typing_extensions import Annotated
 # ===== CONSTANTS =====
 
 SNYK_V1_API_BASE_URL        = 'https://snyk.io/api/v1'
+SNYK_V1_API_BASE_URL_AU     = 'https://snyk.au.io/api/v1'
+SNYK_V1_API_BASE_URL_EU     = 'https://snyk.eu.io/api/v1'
 SNYK_REST_API_BASE_URL      = 'https://api.snyk.io/rest'
+SNYK_REST_API_BASE_URL_AU   = 'https://api.au.snyk.io/rest'
+SNYK_REST_API_BASE_URL_EU   = 'https://api.eu.snyk.io/rest'
 SNYK_REST_API_VERSION       = '2023-11-27~beta'
 SNYK_HIDDEN_API_BASE_URL    = 'https://api.snyk.io/hidden'
+SNYK_HIDDEN_API_BASE_URL_AU = 'https://api.au.snyk.io/hidden'
+SNYK_HIDDEN_API_BASE_URL_EU = 'https://api.eu.snyk.io/hidden'
 SNYK_HIDDEN_API_VERSION     = '2023-04-02~experimental'
 SNYK_API_TIMEOUT_DEFAULT    = 90
 
@@ -40,6 +46,11 @@ def main(
             typer.Argument(
                 help='Snyk API token, or set as environment variable',
                 envvar='SNYK_TOKEN')],
+    tenant:
+        Annotated[
+            str,
+            typer.Option(
+                help="Defaults to US tenant, add 'eu' or 'au'")] = "",
     dry_run:
         Annotated[
             bool,
@@ -56,23 +67,29 @@ def main(
     if verbose:
         state["verbose"] = True
 
-    if verify_org_integrations(snyk_token, org_id):
-        targets = get_all_targets(snyk_token, org_id)
+    if tenant not in ('', 'au', 'eu'):
+        print(f"Invalid tenant: {tenant}")
+        print("Must me either 'eu' or 'au'")
+        return
+
+    if verify_org_integrations(snyk_token, org_id, tenant=tenant):
+        targets = get_all_targets(snyk_token, org_id, tenant=tenant)
 
         if include_github_targets:
-            targets.extend(get_all_targets(snyk_token, org_id, origin='github'))
+            targets.extend(get_all_targets(snyk_token, org_id, origin='github', tenant=tenant))
 
         if (dry_run):
             dry_run_targets(targets)
         else:
-            migrate_targets(snyk_token, org_id, targets)
+            migrate_targets(snyk_token, org_id, targets, tenant=tenant)
 
-def verify_org_integrations(snyk_token, org_id):
+def verify_org_integrations(snyk_token, org_id, tenant=''):
     """Helper function to make sure the Snyk Organization has the relevant github integrations set up
 
     Args:
         snyk_token (str): Snyk API token
         org_id (str): Snyk Organization ID
+        tenant (str, optional): Snyk tenant
 
     Returns:
         bool: _description_
@@ -81,41 +98,53 @@ def verify_org_integrations(snyk_token, org_id):
         'Authorization': f'token {snyk_token}'
     }
 
-    url = f"{SNYK_V1_API_BASE_URL}/org/{org_id}/integrations"
+    base_url = SNYK_V1_API_BASE_URL
 
-    response = requests.request(
-        'GET',
-        url,
-        headers=headers,
-        timeout=SNYK_API_TIMEOUT_DEFAULT
-    )
+    if tenant == 'au':
+        base_url = SNYK_V1_API_BASE_URL_AU
+    if tenant == 'eu':
+        base_url = SNYK_V1_API_BASE_URL_EU
 
-    if response.status_code != 200:
-        print(f"Unable to retrieve integrations for Snyk org: {org_id}, reason: {response.status_code}")
+    url = f"{base_url}/org/{org_id}/integrations"
+
+    try:
+        response = requests.request(
+            'GET',
+            url,
+            headers=headers,
+            timeout=SNYK_API_TIMEOUT_DEFAULT
+        )
+    except requests.ConnectionError:
+        print(f"Unable to connect to {base_url}")
         return False
+    else:
+        if response.status_code != 200:
+            print(f"Unable to retrieve integrations for Snyk org: {org_id}, reason: {response.status_code}")
+            return False
 
-    integrations = json.loads(response.content)
+        integrations = json.loads(response.content)
 
-    if ('github-enterprise' not in integrations and
-        'github' not in integrations):
+        if ('github-enterprise' not in integrations and
+            'github' not in integrations):
 
-        print(f"No GitHub or GitHub Enterprise integration detected for Snyk Org: {org_id}")
-        return False
+            print(f"No GitHub or GitHub Enterprise integration detected for Snyk Org: {org_id}")
+            return False
 
-    if ('github-cloud-app' not in integrations):
+        if ('github-cloud-app' not in integrations):
 
-        print(f"No GitHub Cloud App integration detected for Snyk Org: {org_id}, please set up before migrating GitHub or GitHub Enterprise targets")
-        return False
+            print(f"No GitHub Cloud App integration detected for Snyk Org: {org_id}, please set up before migrating GitHub or GitHub Enterprise targets")
+            return False
 
-    return True
+        return True
 
-def get_all_targets(snyk_token, org_id, origin='github-enterprise'):
+def get_all_targets(snyk_token, org_id, origin='github-enterprise', tenant=''):
     """Helper function to retrieve targets in an org
 
     Args:
         snyk_token (str): Snyk API token
         org_id (str): Snyk Organization ID
         origin (str, optional): Filter to retrieve targets of a certain origin. Defaults to 'github-enterprise'.
+        tenant (str, optional): Snyk tenant
 
     Returns:
         list: github targets in a snyk org
@@ -127,7 +156,14 @@ def get_all_targets(snyk_token, org_id, origin='github-enterprise'):
         'Authorization': f'token {snyk_token}'
     }
 
-    url = f'{SNYK_REST_API_BASE_URL}/orgs/{org_id}/targets?version={SNYK_REST_API_VERSION}&limit=100&origin={origin}&excludeEmpty=false'
+    base_url = SNYK_REST_API_BASE_URL
+
+    if tenant == 'au':
+        base_url = SNYK_REST_API_BASE_URL_AU
+    if tenant == 'eu':
+        base_url = SNYK_REST_API_BASE_URL_EU
+
+    url = f'{base_url}/orgs/{org_id}/targets?version={SNYK_REST_API_VERSION}&limit=100&origin={origin}&excludeEmpty=false'
 
     while True:
         response = requests.request(
@@ -143,7 +179,7 @@ def get_all_targets(snyk_token, org_id, origin='github-enterprise'):
 
         if 'next' not in response_json['links'] or response_json['links']['next'] == '':
             break
-        url = f"{SNYK_REST_API_BASE_URL}/{response_json['links']['next']}"
+        url = f"{base_url}/{response_json['links']['next']}"
 
     return targets
 
@@ -159,21 +195,30 @@ def dry_run_targets(targets):
     print()
     print(f"Total Targets: {len(targets)}")
 
-def migrate_targets(snyk_token, org_id, targets):
+def migrate_targets(snyk_token, org_id, targets, tenant=''):
     """Helper function to migrate list of github and github-enterprise targets to github-cloud-app
 
     Args:
         snyk_token (str): Snyk API token
         org_id (str): Snyk Organization ID
         targets (list): List of targets to be migrated
+        tenant (str, optional): Snyk tenant
     """
+
+    base_url = SNYK_HIDDEN_API_BASE_URL
+
+    if tenant == 'au':
+        base_url = SNYK_HIDDEN_API_BASE_URL_AU
+    if tenant == 'eu':
+        base_url = SNYK_HIDDEN_API_BASE_URL_EU
+
     headers = {
         'Content-Type': 'application/vnd.api+json',
         'Authorization': f'token {snyk_token}'
     }
 
     for target in targets:
-        url = f"{SNYK_HIDDEN_API_BASE_URL}/orgs/{org_id}/targets/{target['id']}?version={SNYK_HIDDEN_API_VERSION}"
+        url = f"{base_url}/orgs/{org_id}/targets/{target['id']}?version={SNYK_HIDDEN_API_VERSION}"
 
         body = json.dumps({
             "data": {
